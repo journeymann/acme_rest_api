4b0f47d3ddea52cade7dc7a17d8e98822ae27547
Casmon Gordon <48144124+journeymann@users.noreply.github.com>
2023-11-08 13:30:05 -0500

    Create settings.py
    initial revision

---
+++ settings.py
@@ -0,0 +1,11 @@
+# accountapi/settings.py
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "accounts",
+]
